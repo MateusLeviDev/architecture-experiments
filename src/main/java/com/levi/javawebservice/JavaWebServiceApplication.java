cd81eac562b5baf1c088242a29c381a25451b6e8
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-04-06 17:08:51 -0300

    initial commit

---
+++ src/main/java/com/levi/javawebservice/JavaWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.levi.javawebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebServiceApplication.class, args);
+	}
+
+}
