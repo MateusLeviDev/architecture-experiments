ce1a03088cfa1a4fc228a1c92d052051a37b85f2
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-04-08 17:00:47 -0300

    banco de dados h2

--- src/main/java/resources/UserResource.java
+++ src/main/java/resources/UserResource.java
@@ -1,10 +1,12 @@
 package resources;
 
-import entities.User;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import entities.User;
 
 @RestController
 @RequestMapping(value = "/users")
