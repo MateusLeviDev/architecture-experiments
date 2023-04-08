982d1579bea05e4cfbfed7829ede48de71e2fa9e
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-04-08 13:03:00 -0300

    Entidade User e seu resource

---
+++ src/main/java/resources/UserResource.java
@@ -0,0 +1,18 @@
+package resources;
+
+import entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    // método de endpoint para acesso aos usuários
+    @GetMapping //para especificar que sse método responde a req do tipo get
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
