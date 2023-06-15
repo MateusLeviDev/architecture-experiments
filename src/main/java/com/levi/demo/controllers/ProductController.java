5fa3de03a410a6fc8fabd2d96f5534f72fb0c9f0
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-15 18:47:08 -0300

    feat: create ProductController.java

---
+++ src/main/java/com/levi/demo/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.levi.demo.controllers;
+
+import com.levi.demo.dtos.ProductRecordDTO;
+import com.levi.demo.models.ProductModel;
+import com.levi.demo.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDTO, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+}
