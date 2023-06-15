364a8e4ed4684987e44929251d0bbe0f48c5dbe4
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-15 18:47:31 -0300

    feat: create ProductRepository.java

---
+++ src/main/java/com/levi/demo/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.levi.demo.repositories;
+
+import com.levi.demo.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
