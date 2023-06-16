318b1d9049ea1f3c65f5de392e612b273d5a637b
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-06-16 18:34:53 -0300

    feat: implement @HATEOAS. Four layer os web development

--- src/main/java/com/levi/demo/controllers/ProductController.java
+++ src/main/java/com/levi/demo/controllers/ProductController.java
@@ -14,6 +14,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 public class ProductController {
 
@@ -29,16 +32,24 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts() {
-        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+        List<ProductModel> productsList = productRepository.findAll();
+        if(!productsList.isEmpty()) {
+            for(ProductModel product : productsList) {
+                UUID id = product.getIdProduct();
+                product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productsList);
     }
 
     @GetMapping("/products/{id}")
     public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id")UUID id) {
-        Optional<ProductModel> product0 = productRepository.findById(id);
-        if (product0.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.OK).body("Product not found.");
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if(productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
         }
-        return ResponseEntity.status(HttpStatus.OK).body(product0.get());
+        productO.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Products List"));
+        return ResponseEntity.status(HttpStatus.OK).body(productO.get());
     }
 
     @PutMapping("/products/{id}")
