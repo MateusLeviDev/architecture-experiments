c66c3df8e72f3a2be77c865560e9f669a7be7f47
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-07-13 12:13:04 -0300

    feat: removing content and restarting repo

--- src/main/java/com/levi/demo/dtos/ProductRecordDTO.java
+++
@@ -1,9 +0,0 @@
-package com.levi.demo.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-import java.math.BigDecimal;
-
-public record ProductRecordDTO(@NotBlank String name,@NotNull BigDecimal value) {
-}
